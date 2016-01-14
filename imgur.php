124fa0db6328c3f7f271c998fedf7a79566c8beb
Dominic Cuneo <cuneo94@gmail.com>
2016-01-14 13:27:30 -0700

    added persona

---
+++ imgur.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title> My web page </title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+				<p> The typical person that uses Imgur is a Judgemental jerk pretending to not be a judgemental jerk. </p>
+		<h2>Use case</h2>
+		<p></p>
+	</body
+
+</html
