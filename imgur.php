98fd0656e5a149e49858a6979f526e6dd5ee8da7
Dominic Cuneo <cuneo94@gmail.com>
2016-01-14 15:11:28 -0700

    changed use case <p> and added goal

--- imgur.php
+++ imgur.php
@@ -2,13 +2,15 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title> My web page </title>
+		<title> Imgur Data project </title>
 	</head>
 	<body>
 		<h1>Persona</h1>
 				<p> The typical person that uses Imgur is a Judgemental jerk pretending to not be a judgemental jerk. </p>
 		<h2>Use case</h2>
-		<p>People use imgur to post pictures, gifs, and videos for comical reasons,support for hard times,and help with real life issues.This creates a interesting community of scumbags,comics,degenerates,and some actully decent people.What helps make this community what it is the comment system allowing users to give advice or be a real scumbag from anywhere at anytime.  </p>
+		<p>People use imgur to post pictures, gifs, and videos for comical reasons,support for hard times,and help with real life issues.This creates a interesting community of scumbags,comics,degenerates,and some actully decent people.What helps make this community what it is the comment system allowing users to give advice or be a real scumbag from anywhere at anytime.Along with the comment system people are able to up vote, down vote and favorite. Up vote to show that you like the post and down vote to like it. Favorite the post to store it for easy reference at a later date </p>
+		<h3>Goal</h3>
+		<p> </p>
 	</body>
 
 </html>
