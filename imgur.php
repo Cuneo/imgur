8807b8743f396064cfd9bc8a08e982729a41de17
Dominic Cuneo <cuneo94@gmail.com>
2016-01-14 14:00:07 -0700

    started use case

--- imgur.php
+++ imgur.php
@@ -8,7 +8,7 @@
 		<h1>Persona</h1>
 				<p> The typical person that uses Imgur is a Judgemental jerk pretending to not be a judgemental jerk. </p>
 		<h2>Use case</h2>
-		<p></p>
-	</body
+		<p>People use imgur to post pictures, gifs, and videos for comical reasons,support for hard times,and help with real life issues.This creates a interesting community of scumbags,comics,degenerates,and some actully decent people.What helps make this community what it is the comment system allowing users to give advice or be a real scumbag from anywhere at anytime.  </p>
+	</body>
 
-</html
+</html>
