9eb020e7c81c7e4c508ac1e4e07eb975d65251ce
Dominic Cuneo <cuneo94@gmail.com>
2016-01-20 09:51:10 -0700

    fixed persona and use case

--- imgur.php
+++ imgur.php
@@ -6,12 +6,44 @@
 	</head>
 	<body>
 		<h1>Persona</h1>
-				<p> The typical person that uses Imgur is a Judgemental jerk pretending to not be a judgemental jerk. </p>
+				<p> The typical person that uses Imgur is a Judgemental jerk pretending to not be a judgemental jerk.</p>
+		<ul>
+			<li>Name: Mongy Gorp a.k.a 420noscopejesusXxxXXx69</li>
+			<li>Race: unknown(Possible cave troll or rawhead) </li>
+			<li>Appearance: unknown (probably very ugly and definitely smelly)  </li>
+			<br><ul>Skills:
+				<li>	Creating the woes and lamination of your females </li>
+				<li>	Making you angry</li>
+				<li>	Making you contemplate murder</li>
+			</ul>
+			<br><ul>Likes:
+			<li> Clubbing baby seals</li>
+			<li>Living under bridges </li>
+				<li>	Charging others for HIS rent</li>
+				<li>	Telling others to be better at life </li>
+				<li>	Being unemployed </li>
+				<li>	Feasting on others unhappiness </li>
+				<li>	Eating babies and various livestock</li>
+				<li>	Racism</li>
+				<li>	Crating dank memes</li>
+				<li>	Being a troll</li>
+			</ul>
+			<br><ul>Dislikes:
+				<li>	The Billy Goats Gruff</li>
+				<li>	Wholesomeness</li>
+				<li>	Others well being </li>
+				<li>	Happiness</li>
+				<li>	Giving to NPR</li>
+				<li>	anything you like (unless mentioned in likes)</li>
+			</ul>
+		</ul>
+		<p>Current status: tolling on imgur and various subreddits.</p>
 		<h2>Use case</h2>
-		<p>People use imgur to post pictures, gifs, and videos for comical reasons,support for hard times,and help with real life issues.This creates a interesting community of scumbags,comics,degenerates,and some actually decent people.</p>
-		<p>What helps make this community what it is the comment system allowing users to give advice or be a real scumbag from anywhere at anytime. Along with the comment system people are able to up vote, down vote and favorite. Up vote to show that you like the post and down vote to like it. Favorite the post to store it for easy reference at a later date </p>
- 		<p>The goal of Imgur is to be a place where like minded people can come together and share their interest and expose people to new ones.<br>To obtain this goal imgur has an app on IOs android and a website. Allow you to post comment and vote anywhere you  want to and at any time.</p>
 
+
+
+		<p>To use Imgur you must click on sign up in the top right corner. A window will pop up asking for a user name that must be four or more letters. A email will be next followed by a password and the retype password.Alternatively it lets you sign up with facebook,google+,and twitter.</p></p>Now you can look at the various post others have created by  using the search bar.I the top right corner click on the menu icon represented by three lines on top of each other. This creates a droplist listing we'rehiring,blog,about imgur,imgur store,imgur apps,developer api,need help?,terms,privacy ad choices and to like on facebook and follow on twitter. Click  on the shuffle icon represented by two arrows crossing for Imgur to load a page of random posts.
+			Click on the blue icon in the top left corner to upload images wich gives four options browse computer, enter url,paste from clipboard,drag and drop.If you click on the down arrow on the right side of the blue icon it creates a droplist to upload images, video to gif,make a meme.</p> <p> Once you click on a post you can scroll to view the comments. Above the first comment there is a bar that says submit comment click and start typing your comment.Click the save button underneath it to post the comment. Under the post there is a up arrow a down arrow and a heart. Use respectively to up vote, down vote, and favorite. voting increases or decrease the points counter next to the heart. Favorite will save that post to your profile for easy reference. Now go shower the internet in dank memes. </p>
 	</body>
 
 </html>
---
+++ mySQL/mysql.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS updownvote;
+DROP TABLE IF EXISTS favorite;
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile (
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	email VARCHAR(128)NOT NULL,
+	username VARCHAR(40)NOT NULL,
+	UNIQUE (email),
+	UNIQUE (username),
+	PRIMARY KEY (profileId)
+);
+CREATE TABLE post (
+	postId      INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileId   INT UNSIGNED NOT NULL,
+	postContent VARCHAR(255) NOT NULL,
+	postDate    DATETIME NOT NULL,
+	INDEX (profileId),
+	FOREIGN KEY (profileId) REFERENCES profile (profileId),
+	PRIMARY KEY (postId)
+);
+CREATE TABLE comment (
+	postId INT UNSIGNED NOT NULL,
+	profileId INT UNSIGNED NOT NULL,
+	comment VARCHAR(255) NOT NULL,
+	commentDate DATETIME NOT NULL,
+	INDEX (profileId),
+	INDEX (postId),
+	FOREIGN KEY (profileId) REFERENCES profile(profileId),
+	FOREIGN KEY (postId) REFERENCES post(postId),
+	PRIMARY KEY (profileId, postId)
+);
+CREATE TABLE favorite (
+	profileId INT UNSIGNED NOT NULL,
+	postId INT UNSIGNED NOT NULL,
+	favoriteDate DATETIME NOT NULL,
+	INDEX (profileId),
+	INDEX (postId),
+	FOREIGN KEY (profileId) REFERENCES profile(profileId),
+	FOREIGN KEY (postId) REFERENCES post(postId),
+	PRIMARY KEY (profileId, postId)
+);
+CREATE TABLE updownvote (
+	profileId INT UNSIGNED NOT NULL,
+	postId INT UNSIGNED NOT NULL,
+	updownvoteDate DATETIME NOT NULL,
+	INDEX (profileId),
+	INDEX (postId),
+	FOREIGN KEY (profileId) REFERENCES profile(profileId),
+	FOREIGN KEY (postId) REFERENCES post(postId),
+	PRIMARY KEY (profileId, postId)
+);
