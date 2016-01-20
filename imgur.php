2730bfa750c40d90adeff9ee8109eccaf1777159
Dominic Cuneo <cuneo94@gmail.com>
2016-01-20 10:19:24 -0700

    added some spacing for case use

--- imgur.php
+++ imgur.php
@@ -11,12 +11,12 @@
 			<li>Name: Mongy Gorp a.k.a 420noscopejesusXxxXXx69</li>
 			<li>Race: unknown(Possible cave troll or rawhead) </li>
 			<li>Appearance: unknown (probably very ugly and definitely smelly)  </li>
-			<br><ul>Skills:
+			<br> <ul>Skills:
 				<li>	Creating the woes and lamination of your females </li>
 				<li>	Making you angry</li>
 				<li>	Making you contemplate murder</li>
 			</ul>
-			<br><ul>Likes:
+			<br> <ul>Likes:
 			<li> Clubbing baby seals</li>
 			<li>Living under bridges </li>
 				<li>	Charging others for HIS rent</li>
@@ -28,7 +28,7 @@
 				<li>	Crating dank memes</li>
 				<li>	Being a troll</li>
 			</ul>
-			<br><ul>Dislikes:
+			<br> <ul>Dislikes:
 				<li>	The Billy Goats Gruff</li>
 				<li>	Wholesomeness</li>
 				<li>	Others well being </li>
@@ -41,8 +41,11 @@
 		<h2>Use case</h2>
 
 		<p> The goal of Imgur is to create funny memes and pictures and rate them by up/down voting and commenting</p>
+
 		<p>To use Imgur you must click on sign up in the top right corner. A window will pop up asking for a user name that must be four or more letters. A email will be next followed by a password and the retype password.Alternatively it lets you sign up with facebook,google+,and twitter.</p>
+
 		<p>Now you can look at the various post others have created by  using the search bar.I the top right corner click on the menu icon represented by three lines on top of each other. This creates a droplist listing we'rehiring,blog,about imgur,imgur store,imgur apps,developer api,need help?,terms,privacy ad choices and to like on facebook and follow on twitter. Click  on the shuffle icon represented by two arrows crossing for Imgur to load a page of random posts.Click on the blue icon in the top left corner to upload images which gives four options browse computer, enter url,paste from clipboard,drag and drop.If you click on the down arrow on the right side of the blue icon it creates a droplist to upload images, video to gif,make a meme.</p>
+
 		<p> Once you click on a post you can scroll to view the comments. Above the first comment there is a bar that says submit comment click and start typing your comment.Click the save button underneath it to post the comment. Under the post there is a up arrow a down arrow and a heart. Use respectively to up vote, down vote, and favorite. voting increases or decrease the points counter next to the heart. Favorite will save that post to your profile for easy reference. Now go shower the internet in dank memes. </p>
 	</body>
 
