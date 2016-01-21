5d18a5f1f5e4048615b81495be2c76a11470e6f9
Dominic Cuneo <cuneo94@gmail.com>
2016-01-21 10:04:01 -0700

    added class profile

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -11,27 +11,58 @@
  *a profile holds all the information needed to allow a user or customer to  create an account
  *with the website.
  *@author   6 of 13, tertiary adjunct of unimatrix 1483 <6 of 13, tertiary adjunct of unimatrix 1483@thecollective.borg
- **/
+ */
 
 class Profile {
 	/**
 	 * id for this profile; is primary key
-	 *@var int $profileId
-	 **/
+	 * @var int $profileId
+	 */
 	private $profileId;
 	/**
 	 *Username, a unique name that the user creates
-	 * @var varchar $username
-	 **/
+	 * @var string $username
+	 */
 	private $username;
 	/**
 	 * its an email
-	 * @var varchar
-	 */
+	 * @var string
 	private $email;
-	/**
+	 * /**
 	 * accessor method for profile id
 	 *
-	 * @return int val
+	 * @return int value of profile id
 	 */
+	public function getProfileId() {
+		return ($this->profileId);
+	}
+
+	/**
+	 * mutator method for profile id
+	 *
+	 * @param int $newProfileId is new value or profile id
+	 * @throws InvalidArgumentException if $newprofileId is not a integer
+	 * @throws RangeException if profile id is negative
+	 */
+	public function setProfileId($newProfileId) {
+		$newProfileId = filter_var($newProfileId, FILTER_VALIDATE_INT);
+		if($newProfileId === false) {
+			throw(new InvalidArgumentException("profile id is not an integer punk"));
+		}
+		if($newProfileId <= 0) {
+			throw(new RangeException("proflie id must be positive fool"));
+		}
+	}
+
+	/**
+	 * @param $newemail
+	 * @throws InvalidArgumentException $newemail is not a string
+	 */
+	public function setemail($newemail) {
+		$newemail = filter_var($newemail, FILTER_SANITIZE_EMAIL);
+		if ($newemail === false){
+			throw(new InvalidArgumentException("email is not a string please try again"));
+		}
+
+	}
 }
